716897fc895990303636c49b05d4feebdf611e75
stdeshmukh@mitaoe.ac.in <stdeshmukh@mitaoe.ac.in>
2020-04-11 13:01:06 +0530

    Updates

---
+++ precache-manifest.79287eef283daa399fd1194ddf9481e8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b818c3d33d0c3eedf67e41ff06cf22ee",
+    "url": "/covid_19/index.html"
+  },
+  {
+    "revision": "f467d055c9c3b3fb8049",
+    "url": "/covid_19/static/css/2.723bd367.chunk.css"
+  },
+  {
+    "revision": "84fc29fed7e0ed9e2cec",
+    "url": "/covid_19/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f467d055c9c3b3fb8049",
+    "url": "/covid_19/static/js/2.72e9eb02.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid_19/static/js/2.72e9eb02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84fc29fed7e0ed9e2cec",
+    "url": "/covid_19/static/js/main.4d332f31.chunk.js"
+  },
+  {
+    "revision": "69d0d825536a8aed78f3",
+    "url": "/covid_19/static/js/runtime-main.49f4ac31.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/covid_19/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/covid_19/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/covid_19/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/covid_19/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/covid_19/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
